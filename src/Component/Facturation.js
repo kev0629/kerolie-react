1a9ef825bb379bbd39e93b8577bace023374f61a
Kevin Saffioti <kevin.saffioti@gmail.com>
2020-10-19 17:55:40 +0200

    Update Facturation.js

--- src/Component/Facturation.js
+++ src/Component/Facturation.js
@@ -5,7 +5,27 @@ import './assets/css/style.css'
 function Facturation(){
     return(
         <div className="contener">
-            <h1>Section Facturation</h1>
+            <h1>Facturation</h1>
+            <div>
+                <input type="checkbox"  checked/>
+                <label for="scales">Devis</label>
+                <input type="checkbox"  checked/>
+                <label for="scales">enfants</label>
+                <input type="checkbox"  checked/>
+                <label for="scales">Organisme</label>
+                <input type="checkbox"  checked/>
+                <label for="scales">Seconde prestation</label>
+                <input type="checkbox"  checked/>
+                <label for="scales">Déplacement</label>
+            </div>
+            <div>
+                <input type="date" id="date" value=""></input>
+            </div>
+            <div>
+                <input type="checkbox"  checked/>
+                <label for="scales">Patient enregistré</label>  
+            </div>
+           
         </div>
         
     )
