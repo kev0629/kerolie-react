29e9780dab62371f294f1a8e9f58702dd7a63e32
Kevin Saffioti <kevin.saffioti@gmail.com>
2020-11-04 15:34:53 +0100

    Update Facturation.js

--- src/Component/Facturation.js
+++ src/Component/Facturation.js
@@ -6,26 +6,27 @@ function Facturation(){
     return(
         <div className="contener">
             <h1>Facturation</h1>
-            <div>
-                <input type="checkbox"  checked/>
-                <label for="scales">Devis</label>
-                <input type="checkbox"  checked/>
-                <label for="scales">enfants</label>
-                <input type="checkbox"  checked/>
-                <label for="scales">Organisme</label>
-                <input type="checkbox"  checked/>
-                <label for="scales">Seconde prestation</label>
-                <input type="checkbox"  checked/>
-                <label for="scales">Déplacement</label>
-            </div>
-            <div>
-                <input type="date" id="date" value=""></input>
-            </div>
-            <div>
-                <input type="checkbox"  checked/>
-                <label for="scales">Patient enregistré</label>  
-            </div>
-           
+            <form>
+                    <input type="checkbox" />
+                    <label for="scales">Devis</label>
+                    <input type="checkbox"  />
+                    <label for="scales">enfants</label>
+                    <input type="checkbox"  />
+                    <label for="scales">Organisme</label>
+                    <input type="checkbox"  />
+                    <label for="scales">Seconde prestation</label>
+                    <input type="checkbox"  />
+                    <label for="scales">Déplacement</label>
+                    <div>
+                        <input type="date" id="date" value=""></input>
+                    </div>
+                    <div>
+                        <input type="checkbox" />
+                        <label for="scales">Patient enregistré</label>
+                    </div>
+                    <input ></input>
+                <input type="submit" value="Facture" />
+            </form>
         </div>
         
     )
