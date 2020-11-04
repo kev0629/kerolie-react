73583f37878b0cfcff6f99d1552f73fafb8f43af
kevin saffioti <kevin.saffioti@gmail.com>
2020-11-04 08:07:36 +0100

    update with SUI

--- src/Component/Facturation.js
+++ src/Component/Facturation.js
@@ -1,32 +1,41 @@
 import React from 'react';
+import { Input, Checkbox, Button, Form} from 'semantic-ui-react'
 
 import './assets/css/style.css'
 
 function Facturation(){
     return(
+      <Form>
         <div className="contener">
             <h1>Facturation</h1>
-            <div>
-                <input type="checkbox"  checked/>
-                <label for="scales">Devis</label>
-                <input type="checkbox"  checked/>
-                <label for="scales">enfants</label>
-                <input type="checkbox"  checked/>
-                <label for="scales">Organisme</label>
-                <input type="checkbox"  checked/>
-                <label for="scales">Seconde prestation</label>
-                <input type="checkbox"  checked/>
-                <label for="scales">Déplacement</label>
-            </div>
-            <div>
-                <input type="date" id="date" value=""></input>
-            </div>
-            <div>
-                <input type="checkbox"  checked/>
-                <label for="scales">Patient enregistré</label>  
-            </div>
-           
-        </div>
+            <Form.Field>
+                <Checkbox label='Devis' />
+                <Checkbox label='enfants' />
+                <Checkbox label='Organisme' />
+                <Checkbox label='Seconde prestation' />
+                <Checkbox label='Déplacement' />
+            </Form.Field>
+            <input type="date" id="date" value=""></input>
+            <Form.Field>
+              <Checkbox toggle label='Client enregistré' />
+            </Form.Field>
+            <Form.Field>
+              <label>Nom</label>
+              <input placeholder='Nom' />
+            </Form.Field>
+            <Form.Field>
+              <label>Adresse</label>
+              <input placeholder='Adresse' />
+            </Form.Field>
+            <Form.Field>
+              <Form.Input label='Code postal' placeholder='Code postal' width={2}/>
+              <Form.Input label='Ville' placeholder='Ville' width={2}/>
+            </Form.Field>
+            <Form.Field>
+            <Button type='submit'>Submit</Button>
+            </Form.Field>`
+          </div>
+        </Form>
         
     )
 }
